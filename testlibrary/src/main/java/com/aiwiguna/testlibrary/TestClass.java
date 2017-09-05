0447874d4ec3ed9b6cfdd64465e86e21ec5c8eda
Antoni Wiguna <antoni.wiguna@gmail.com>
2017-09-05 09:55:18 +0700

    init

---
+++ testlibrary/src/main/java/com/aiwiguna/testlibrary/TestClass.java
@@ -0,0 +1,14 @@
+package com.aiwiguna.testlibrary;
+
+/**
+ * Created by Android Studio.<br/>
+ * User: Antoni<br/>
+ * Date: 9/5/2017<br/>
+ * Time: 09:49<br/>
+ **/
+public class TestClass {
+
+    public static String appendString(String text1, String text2) {
+        return text1 + text2;
+    }
+}
